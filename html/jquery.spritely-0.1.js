34092de2c33a1005b01040285247bf787c70c3de
michael <mkigotho2@gmail.com>
2015-08-20 07:44:45 +0300

    documentatiopn for the script

---
+++ html/jquery.spritely-0.1.js
@@ -0,0 +1,13 @@
+/*
+ * jQuery spritely - Sprite up the web!
+ * http://spritely.net/
+ *
+ * Latest version:
+ * http://spritely.net/download/
+ *
+ * Copyright 2010, Peter Chater, Artlogic Media Ltd, http://www.artlogic.net/
+ * Dual licensed under the MIT or GPL Version 2 licenses.
+ */
+(function($) {
+	$._spritely = {
+		
