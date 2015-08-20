ab79e5cddc3889fe85fed6acf1f2fe5a7c9648cf
michael <mkigotho2@gmail.com>
2015-08-20 07:45:18 +0300

    declare methods and variables to be used

--- html/jquery.spritely-0.1.js
+++ html/jquery.spritely-0.1.js
@@ -10,4 +10,27 @@
  */
 (function($) {
 	$._spritely = {
-		
+		// shared methods and variables used by spritely plugin
+		animate: function(options) {
+			var el = $(options.el);
+			var el_id = el.attr('id');
+			if (!$._spritely.instances) {
+				$._spritely.instances = {};
+			}
+			if (!$._spritely.instances[el_id]) {
+				$._spritely.instances[el_id] = {current_frame: -1};
+			}
+			var instance = $._spritely.instances[el_id];
+			if (options.type == 'sprite') {
+				var frames;
+				var animate = function(el) {
+					var w = options.width, h = options.height;
+					if (!frames) {
+						frames = [];
+						total = 0
+						for (var i = 0; i < options.no_of_frames; i ++) {
+							frames[frames.length] = (0 - total);
+							total += w;
+						}
+					}
+				
